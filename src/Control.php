dcc9a4ebb8795067ebc44958b7b026858f1474ea
Edikowy <edikowy@vp.pl>
2020-02-14 00:51:34 +0100

    Control wstęp

--- src/Control.php
+++ src/Control.php
@@ -11,21 +11,41 @@ class Control {
     public function __construct() {}
     public function sluchacz() {
         if ($_GET) {
-            print_r($_GET);
+            print_r($_SERVER['QUERY_STRING']);
             echo '</br>';
-//             switch ($_GET) {
-//                 case 'link':
-//                     echo 'link</br>';
-//                     break;
+            switch ($_SERVER['QUERY_STRING']) {
+                case 'linki=1':
+                    echo 'link 1</br>';
+                    break;
                 
-//                 case 'dupa':
-//                     echo 'dupa</br>';
-//                     break;
+                case 'linki=2':
+                    echo 'link 2</br>';
+                    break;
                     
-//                 default:
-//                     echo 'defaulty</br>';
-//                     break;
-//             }
+                case 'linki=3':
+                    echo 'link 3</br>';
+                    break;
+                    
+                case 'linki=4':
+                    echo 'link 4</br>';
+                    break;
+                    
+                case 'linki=5':
+                    echo 'link 5</br>';
+                    break;
+                    
+                case 'linki=6':
+                    echo 'link 6</br>';
+                    break;
+                    
+                case 'dupa':
+                    echo 'dupa</br>';
+                    break;
+                    
+                default:
+                    echo 'defaulty</br>';
+                    break;
+            }
         }
         if ($_POST) {
             print_r($_POST);
