b613280db0a0d1915d673aab472189cdae28e4ad
Edikowy <edikowy@vp.pl>
2020-02-18 15:03:58 +0100

    sluchacz switche poprawione

--- src/Control.php
+++ src/Control.php
@@ -11,67 +11,50 @@ class Control {
     public function __construct() {}
     public function sluchacz() {
         if ($_GET) {
-            print_r($_SERVER['QUERY_STRING']);
-            echo '</br>';
-            switch ($_SERVER['QUERY_STRING']) {
-                case 'linki=1':
+            $util = new Util();
+            $view = new View();
+            $linki = (int)$_GET['linki'];
+            switch ($linki) {
+                case 1:
                     echo 'link 1</br>';
-                    break;
+                    $util ->serverIdent2();
+                break;
                 
-                case 'linki=2':
-                    $view = new View();
-                    echo '</br>';
+                case 2:
+                    echo 'link 2</br>';
                     echo $view -> showFormRegist();
-                    echo '</br>';
-                    break;
-                    
-                case 'linki=3':
-                    $view = new View();
-                    echo '</br>';
                     echo $view -> showFormAdminNewEmail();
-                    echo '</br>';
-                    break;
-                    
-                case 'linki=4':
-                    $view = new View();
-                    echo '</br>';
                     echo $view -> showFormAdminNewPass();
-                    echo '</br>';
-                    break;
-                    
-                case 'linki=5':
-                    $view = new View();
-                    echo '</br>';
                     echo $view -> showFormAdminDel();
-                    echo '</br>';
-                    break;
+                break;
+                
+                case 3:
+                    echo 'link 3</br>';
+                    echo $view -> showFormAdminNewEmail();
+                    echo $view -> showFormAdminNewPass();
+                    echo $view -> showFormAdminDel();
+                break;
                     
-                case 'linki=6':
+                case 4:
+                    echo 'link 4</br>';
+                    echo $view -> showFormRegist();
+                    
+                break;
+                    
+                case 5:
+                    echo 'link 5</br>';
+                break;
+                    
+                case 6:
                     echo 'link 6</br>';
-                    break;
-                    
-                case 'dupa':
-                    echo 'dupa</br>';
-                    break;
-                    
+                break;
+                
                 default:
-                    echo 'defaulty</br>';
-                    break;
+                    echo 'default</br>';
+                break;
             }
         }
-        if ($_POST) {
-            print_r($_POST);
-            echo '</br>';
-//             switch ($_POST) {
-//                 case value:
-//                 ;
-//                 break;
-                
-//                 default:
-//                     ;
-//                 break;
-//             }
-        }
+        if ($_POST) {}
     }
 }
 
